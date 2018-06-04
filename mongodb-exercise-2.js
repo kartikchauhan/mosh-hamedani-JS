ba8c77c74487ce3532aa3c8f12ca096872bb3baf
kartik <chauhan.kartik25@gmail.com>
2018-06-04 11:32:40 +0530

    mongodb exercise-2 done

---
+++ mongodb-exercise-2.js
@@ -0,0 +1,34 @@
+const mongoose = require('mongoose');
+
+mongoose.connect('mongodb://localhost:27017/mongo-exercise-1')
+    .then(() => console.log('connected to the database'))
+    .catch(err => console.log(err));
+
+const courseSchema = mongoose.Schema({
+    tags: [ String ],
+    data: Date,
+    name: String,
+    author: String,
+    isPublished: Boolean,
+    price: Number
+});
+
+const Course = mongoose.model('Course', courseSchema);
+
+async function getCourses()
+{
+    return await Course
+                    .find({ isPublished: true, tags: {$in: ['frontend', 'backend']}})
+                    // .or([{tags: 'backend'}, {tags: 'frontend'}])
+                    .sort({price: -1})
+                    .select('name author');
+}
+
+async function displayCourses()
+{
+    const courses = await getCourses();
+    console.log(courses);
+}
+
+displayCourses();
+
